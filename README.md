7117e08dc9492b731cdd337948bed19b4b0bd9e9
xmjomax <529918222@qq.com>
2018-10-04 16:07:54 +0800

    description

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # miv-sass
 > Scss Common library
 
-## 0.0.3版本更新
+## 0.0.4版本更新
 
 * 更改scss文件名为驼峰命名
 * 删除src目录下的`_index.scss`文件，仅保留一个路口根路径下的`index.scss`
--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-npm version minor && npm publish && npm version patch && git push --tags && git push origin master
+npm publish && git push origin master
